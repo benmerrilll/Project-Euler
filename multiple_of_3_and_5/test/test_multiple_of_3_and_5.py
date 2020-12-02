0197d2b556375ac44d301ca069c097537cdac57a
riccardompesce <riccardompesce@gmail.com>
2020-12-02 23:05:36 +0100

    Added test cases

---
+++ multiple_of_3_and_5/test/test_multiple_of_3_and_5.py
@@ -0,0 +1,7 @@
+from multiple_of_3_and_5 import multiple_of_3_and_5
+
+def test_pe_base_case():
+    """
+    Testing base case as per Project Euler
+    """
+    assert multiple_of_3_and_5(10) == 23
